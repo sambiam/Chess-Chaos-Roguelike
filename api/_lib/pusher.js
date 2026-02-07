9b2a8f19862d37b2d39bcbad57da832bfba89f1f
DougDougGitHub <dougdoug@dougdoug.com>
2026-02-06 18:34:16 -0800

    Visual tweaks

--- api/_lib/pusher.js
+++ api/_lib/pusher.js
@@ -5,8 +5,7 @@ const pusher = new Pusher({
 	key: process.env.PUSHER_KEY,
 	secret: process.env.PUSHER_SECRET,
 	cluster: process.env.PUSHER_CLUSTER,
-	useTLS: true,
-	keepAlive: false
+	useTLS: true
 });
 
 export default pusher;
