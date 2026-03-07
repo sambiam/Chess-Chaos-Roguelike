20ef261b72c6f33d6506a3b3921bde1192afde3b
DougDougGitHub <dougdoug@dougdoug.com>
2026-03-06 18:41:00 -0800

    Minor emote fix

--- src/board-state.js
+++ src/board-state.js
@@ -41,7 +41,7 @@ export const BOARD_EFFECT_OPTIONS = [
     { name: 'misc_5',    emoji: '🌪️' },
     { name: 'misc_6',    emoji: '💰' },
     { name: 'misc_7',    emoji: '🕳️' },
-    { name: 'misc_9',    emoji: '⚡' },
+    { name: 'misc_8',    emoji: '⚡' },
     { name: 'misc_9',    emoji: '☢️' },
 ];
 
